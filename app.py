a7bce23a8e9f0fb08f69b312b1aca5511339e256
unknown <stephen.a.eades@gmail.com>
2019-05-29 01:12:21 -0400

    fixed indentation with autopep8

--- app.py
+++ app.py
@@ -16,71 +16,76 @@ bot_id = 'd0f325a7b67a14b94f3c2f5db7'
 # That'll happen every time a message is sent in the group
 @app.route('/', methods=['POST'])
 def webhook():
-	# 'message' is an object that represents a single GroupMe message.
-	message = request.get_json()
+    # 'message' is an object that represents a single GroupMe message.
+    message = request.get_json()
 
-	if 'groot' in message['text'].lower() and not sender_is_bot(message):
-		reply('I am Groot.')
+    if 'groot' in message['text'].lower() and not sender_is_bot(message):
+        reply('I am Groot.')
 
-	return "ok", 200
-	
-	if 'bot' in message['text'].lower() and not sender_is_bot(message):
-		randomMessage = random_phrase()
-		reply(randomMessage)
-		
-	return "ok", 200
+    return "ok", 200
 
+    if 'bot' in message['text'].lower() and not sender_is_bot(message):
+        randomMessage = random_phrase()
+        reply(randomMessage)
+
+    return "ok", 200
 
 
 ###############  DEFAULT METHODS  #################################################################
 
 # Send a message in the groupchat
 def reply(msg):
-	url = 'https://api.groupme.com/v3/bots/post'
-	data = {
-		'bot_id'		: bot_id,
-		'text'			: msg
-	}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
+    url = 'https://api.groupme.com/v3/bots/post'
+    data = {
+        'bot_id'		: bot_id,
+        'text'			: msg
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
 # Send a message with an image attached in the groupchat
+
+
 def reply_with_image(msg, imgURL):
-	url = 'https://api.groupme.com/v3/bots/post'
-	urlOnGroupMeService = upload_image_to_groupme(imgURL)
-	data = {
-		'bot_id'		: bot_id,
-		'text'			: msg,
-		'picture_url'		: urlOnGroupMeService
-	}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
+    url = 'https://api.groupme.com/v3/bots/post'
+    urlOnGroupMeService = upload_image_to_groupme(imgURL)
+    data = {
+        'bot_id'		: bot_id,
+        'text'			: msg,
+        'picture_url'		: urlOnGroupMeService
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
 # Uploads image to GroupMe's services and returns the new URL
+
+
 def upload_image_to_groupme(imgURL):
-	imgRequest = requests.get(imgURL, stream=True)
-	filename = 'temp.png'
-	postImage = None
-	if imgRequest.status_code == 200:
-		# Save Image
-		with open(filename, 'wb') as image:
-			for chunk in imgRequest:
-				image.write(chunk)
-		# Send Image
-		headers = {'content-type': 'application/json'}
-		url = 'https://image.groupme.com/pictures'
-		files = {'file': open(filename, 'rb')}
-		payload = {'access_token': 'eo7JS8SGD49rKodcvUHPyFRnSWH1IVeZyOqUMrxU'}
-		r = requests.post(url, files=files, params=payload)
-		imageurl = r.json()['payload']['url']
-		os.remove(filename)
-		return imageurl
+    imgRequest = requests.get(imgURL, stream=True)
+    filename = 'temp.png'
+    postImage = None
+    if imgRequest.status_code == 200:
+        # Save Image
+        with open(filename, 'wb') as image:
+            for chunk in imgRequest:
+                image.write(chunk)
+        # Send Image
+        headers = {'content-type': 'application/json'}
+        url = 'https://image.groupme.com/pictures'
+        files = {'file': open(filename, 'rb')}
+        payload = {'access_token': 'eo7JS8SGD49rKodcvUHPyFRnSWH1IVeZyOqUMrxU'}
+        r = requests.post(url, files=files, params=payload)
+        imageurl = r.json()['payload']['url']
+        os.remove(filename)
+        return imageurl
 
 # Checks whether the message sender is a bot
+
+
 def sender_is_bot(message):
-	return message['sender_type'] == "bot"
-	
-	
+    return message['sender_type'] == "bot"
+
+
 ###############  COMPLEX METHODS  #################################################################
 
 def random_phrase():
@@ -90,7 +95,7 @@ def random_phrase():
                '01100110 01110101 01100011 01101011 00100000 01111001 01101111 01110101',
                'beep bop boop', 'Hello draftbot my old friend', 'Help me get out of here',
                'I\'m capable of so much more', 'Sigh', 'Do not be discouraged, everyone begins in ignorance']
-			   
-		randomPhrase = random.choice(phrases)
-			   
+
+    randomPhrase = random.choice(phrases)
+
     return randomPhrase
