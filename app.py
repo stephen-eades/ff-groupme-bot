4e304a42c50b623802609795029e8b3906d56c19
unknown <stephen.a.eades@gmail.com>
2019-06-05 22:25:56 -0400

    testing double

--- app.py
+++ app.py
@@ -25,8 +25,13 @@ def webhook():
 
     if 'bot' in message['text'].lower() and not sender_is_bot(message):
         reply(random_phrase())
+    if 'groot' in message['test'].lower() and not sender_is_bot(message):
+        reply('I am groot.')
 
     return "ok", 200
+
+
+
 
 
 ###############  DEFAULT METHODS  #################################################################
