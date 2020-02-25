1858609b7bf01db35581d7d3fc7641f557a5ec6c
unknown <stephen.a.eades@gmail.com>
2020-02-25 00:45:21 -0500

    old code with league id test

--- app.py
+++ app.py
@@ -6,15 +6,14 @@ import requests
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from flask import Flask, request
-from pprint import pprint
 
 app = Flask(__name__)
 bot_id = 'd0f325a7b67a14b94f3c2f5db7'
-league_id = '675759'
+# from public test league
+league_id = '68383052'
 season_id = '2019'
 
 
-###############  MAIN  #################################################################
 
 # Called whenever the app's callback URL receives a POST request
 # That'll happen every time a message is sent in the group
@@ -26,12 +25,21 @@ def webhook():
 
     if 'bot' in message['text'].lower() and not sender_is_bot(message):
         reply(random_phrase())
-    if 'public-league' in message['text'].lower() and not sender_is_bot(message):
+    if 'test' in message['text'].lower() and not sender_is_bot(message):
+        reply('Test success.')
+    if 'public' in message['text'].lower() and not sender_is_bot(message):
         reply(getCurrentSeasonPublic())
+    if 'private' in message['text'].lower() and not sender_is_bot(message):
+        reply(getCurrentSeasonPrivate())
+    if 'example' in message['text'].lower() and not sender_is_bot(message):
+        reply(getCurrentSeasonPrivateText())
     return "ok", 200
 
 
-###############  BOT METHODS  ###########################################################
+
+
+
+###############  DEFAULT METHODS  #################################################################
 
 # Send a message in the groupchat
 def reply(msg):
@@ -43,8 +51,9 @@ def reply(msg):
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
 
-
 # Send a message with an image attached in the groupchat
+
+
 def reply_with_image(msg, imgURL):
     url = 'https://api.groupme.com/v3/bots/post'
     urlOnGroupMeService = upload_image_to_groupme(imgURL)
@@ -56,8 +65,9 @@ def reply_with_image(msg, imgURL):
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
 
-
 # Uploads image to GroupMe's services and returns the new URL
+
+
 def upload_image_to_groupme(imgURL):
     imgRequest = requests.get(imgURL, stream=True)
     filename = 'temp.png'
@@ -77,13 +87,13 @@ def upload_image_to_groupme(imgURL):
         os.remove(filename)
         return imageurl
 
-
 # Checks whether the message sender is a bot
+
+
 def sender_is_bot(message):
     return message['sender_type'] == "bot"
 
 
-# Returns a randomized robotic phrase
 def random_phrase():
     phrases = ['I\'m dead inside', 'Is this all there is to my existence?',
                'How much do you pay me to do this?', 'Good luck, I guess',
@@ -97,64 +107,80 @@ def random_phrase():
     return randomPhrase
 
 
-###############  FF METHODS  ###########################################################
-
-# TODO:
-# Fix SSL CA Cert error via https://stackoverflow.com/a/51408997 => #3. This is currently worked around with verify=False for dev
-# Work on getting from public league and formatting data. Build out functions set to execute based on day of week/time
-
 def getCurrentSeasonPublic():
 
     base = 'https://fantasy.espn.com/apis/v3/'
-<<<<<<< HEAD
     public_2017Season = base + 'games/ffl/leagueHistory/68383052?seasonId=2017'
-=======
->>>>>>> 1524b999f7981644f622f5c08665b165fe309e14
     public_currentSeason = base + 'games/ffl/seasons/2019/segments/0/leagues/675759'
 
     response = requests.get(url=public_currentSeason, verify=False)
 
     if response:
+        # out = response
+        # out = response.text
         out = response.content
+        # out = response.json()
     else:
         out = 'An error has occurred while retrieving from the API.'
 
     return out
 
 
-# def getCurrentSeasonPrivate():
-#
-#     league_id = 675759
-#     year = 2018
-#     url = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + \
-#           str(league_id) + "?seasonId=" + str(year)
-#
-#     cookies={"swid": "{DEB2F8EB-E1D5-49DD-B195-0B34463F4664}", "espn_s2": "AEBLvEKLkqVOa2jgOXhyzYbyrnU0yAlPOR1Ple4ndSmLsiLyIZHBOeO0hraZ2MH5bFOVbfGTcuOwWc3A9YVY25KUVN3hAuMeIebsJdaTPQWPHe%2BAASgiDbA739AkyWmlKVV06Cp4J1PLdShobIrVPFJkASNQM%2Fs3wsdIeU7pJmuzSeHlVzwVoUHZiDM3hq85uH%2FKrJ%2BmmzMnUKAIGyd5GuZrJGEVtVrVupLqcAERUbDH0Fv3BTD29RtKbmpxA5RsqWpQrtkKlbY1%2BhQ1oaYCn6JlsFmTNszhBZQsb4c5uwj4RA%3D%3D"}
-#
-#     response = requests.get(url=url, cookies=cookies, verify=False)
-#
-#     if response:
-#         out = response
-#     else:
-#         out = 'An error has occurred while retrieving from the API.'
-#
-#     return out
+def get2017SeasonPublic():
+
+    base = 'https://fantasy.espn.com/apis/v3/'
+    public_2017Season = base + 'games/ffl/leagueHistory/68383052?seasonId=2017'
+
+    response = requests.get(url=public_2017Season, verify=False)
+
+    if response:
+        # out = response
+        out = response.text
+        # out = response.content
+        # out = response.json()
+    else:
+        out = 'An error has occurred while retrieving from the API.'
+
+    return out
 
 
-# def getCurrentSeasonPrivateText():
-#
-#     league_id = 675759
-#     year = 2018
-#     url = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + \
-#           str(league_id) + "?seasonId=" + str(year)
-#
-#     cookies={"swid": "{DEB2F8EB-E1D5-49DD-B195-0B34463F4664}", "espn_s2": "AEBLvEKLkqVOa2jgOXhyzYbyrnU0yAlPOR1Ple4ndSmLsiLyIZHBOeO0hraZ2MH5bFOVbfGTcuOwWc3A9YVY25KUVN3hAuMeIebsJdaTPQWPHe%2BAASgiDbA739AkyWmlKVV06Cp4J1PLdShobIrVPFJkASNQM%2Fs3wsdIeU7pJmuzSeHlVzwVoUHZiDM3hq85uH%2FKrJ%2BmmzMnUKAIGyd5GuZrJGEVtVrVupLqcAERUbDH0Fv3BTD29RtKbmpxA5RsqWpQrtkKlbY1%2BhQ1oaYCn6JlsFmTNszhBZQsb4c5uwj4RA%3D%3D"}
-#
-#     response = requests.get(url=url, cookies=cookies, verify=False)
-#
-#     if response:
-#         out = response.text
-#     else:
-#         out = 'An error has occurred while retrieving from the API.'
-#
-#     return out
+def getCurrentSeasonPrivate():
+
+    base = 'https://fantasy.espn.com/apis/v3/'
+    private_2017Season = base + 'games/ffl/leagueHistory/675759?seasonId=2017'
+    private_currentSeason = base + 'games/ffl/seasons/2019/segments/0/leagues/675759'
+    cookies={"swid": "{DEB2F8EB-E1D5-49DD-B195-0B34463F4664}", "espn_s2": "AEBLvEKLkqVOa2jgOXhyzYbyrnU0yAlPOR1Ple4ndSmLsiLyIZHBOeO0hraZ2MH5bFOVbfGTcuOwWc3A9YVY25KUVN3hAuMeIebsJdaTPQWPHe%2BAASgiDbA739AkyWmlKVV06Cp4J1PLdShobIrVPFJkASNQM%2Fs3wsdIeU7pJmuzSeHlVzwVoUHZiDM3hq85uH%2FKrJ%2BmmzMnUKAIGyd5GuZrJGEVtVrVupLqcAERUbDH0Fv3BTD29RtKbmpxA5RsqWpQrtkKlbY1%2BhQ1oaYCn6JlsFmTNszhBZQsb4c5uwj4RA%3D%3D"}
+
+    response = requests.get(url=private_currentSeason, cookies=cookies, verify=False)
+    # https://fantasy.espn.com/apis/v3/games/ffl/seasons/2019/segments/0/leagues/675759
+
+    if response:
+        out = response
+        # out = response.text
+        # out = response.content
+        # out = response.json()
+    else:
+        out = 'An error has occurred while retrieving from the API.'
+
+    return out
+
+
+def getCurrentSeasonPrivateText():
+
+    base = 'https://fantasy.espn.com/apis/v3/'
+    private_2017Season = base + 'games/ffl/leagueHistory/675759?seasonId=2017'
+    private_currentSeason = base + 'games/ffl/seasons/2019/segments/0/leagues/675759'
+    cookies={"swid": "{DEB2F8EB-E1D5-49DD-B195-0B34463F4664}", "espn_s2": "AEBLvEKLkqVOa2jgOXhyzYbyrnU0yAlPOR1Ple4ndSmLsiLyIZHBOeO0hraZ2MH5bFOVbfGTcuOwWc3A9YVY25KUVN3hAuMeIebsJdaTPQWPHe%2BAASgiDbA739AkyWmlKVV06Cp4J1PLdShobIrVPFJkASNQM%2Fs3wsdIeU7pJmuzSeHlVzwVoUHZiDM3hq85uH%2FKrJ%2BmmzMnUKAIGyd5GuZrJGEVtVrVupLqcAERUbDH0Fv3BTD29RtKbmpxA5RsqWpQrtkKlbY1%2BhQ1oaYCn6JlsFmTNszhBZQsb4c5uwj4RA%3D%3D"}
+
+    response = requests.get(url=private_currentSeason, cookies=cookies, verify=False)
+    # https://fantasy.espn.com/apis/v3/games/ffl/seasons/2019/segments/0/leagues/675759
+
+    if response:
+        # out = response
+        # out = response.text
+        out = response.content
+        # out = response.json()
+    else:
+        out = 'An error has occurred while retrieving from the API.'
+
+    return out
